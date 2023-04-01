f14b9b5a05df4373ef6df3d01dd56c30ada24ef9
Håkan Ströberg <hakan.stroberg@codeware.se>
2023-04-01 17:23:19 +0200

    ci: :ferris_wheel: add custom semantic release configuration

---
+++ release.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable unicorn/prefer-module */
+/** @type {import('@types/semantic-release').Options} */
+module.exports = {
+  dryRun: false,
+  ci: true,
+  debug: false,
+
+  tagFormat: 'v${version}',
+  preset: 'angular',
+
+  branches: [
+    // Maintenance releases to existing releases
+    // N.N.x or N.x.x or N.x
+    '+([0-9])?(.{+([0-9]),x}).x',
+
+    // Stable release to default branch
+    'master',
+
+    // Upcoming release
+    'next',
+
+    // Pre-release candidate
+    { name: 'release', prerelease: 'rc' }
+  ],
+
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/exec',
+      {
+        // Update README.md
+        prepareCmd: 'yarn oclif readme'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'README.md'],
+        message:
+          // Add `[skip ci]` in message to prevent triggering a pipeline
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
+};
