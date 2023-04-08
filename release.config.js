9e998973ba49b1c8896fa69d6fff04e8fddf6045
Håkan Ströberg <hakan.stroberg@codeware.se>
2023-04-09 00:01:44 +0200

    fix(docs): :bug: extracted readme should not affect repo by release

--- release.config.js
+++ release.config.js
@@ -40,7 +40,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json', 'README.md'],
+        assets: ['CHANGELOG.md', 'package.json'],
         message:
           // Add `[skip ci]` in message to prevent triggering a pipeline
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
