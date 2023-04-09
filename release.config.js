437b5c32c387fe4e1270ffa4238c7fab953fb843
Håkan Ströberg <hakan.stroberg@codeware.se>
2023-04-09 17:57:18 +0200

    ci: manually dispatch a release

--- release.config.js
+++ release.config.js
@@ -1,3 +1,5 @@
+const { SLACK_DEVOPS_WEBHOOK } = process.env;
+
 /* eslint-disable no-template-curly-in-string */
 /* eslint-disable unicorn/prefer-module */
 /** @type {import('@types/semantic-release').Options} */
@@ -44,6 +46,14 @@ module.exports = {
         message:
           // Add `[skip ci]` in message to prevent triggering a pipeline
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    [
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+        slackWebhook: SLACK_DEVOPS_WEBHOOK
       }
     ]
   ]
